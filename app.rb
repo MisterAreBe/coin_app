ab6cbc9d7a966f925e5a25cc8e5fe2742a4eee8c
Ricky Collins <richardcollins52317@gmail.com>
2018-07-13 15:40:50 -0400

    first

--- app.rb
+++ app.rb
@@ -2,5 +2,34 @@ require 'sinatra'
 require_relative 'coin.rb'
 
 get '/' do
-  ''
+  erb :index, :layout => :layout
+end
+
+
+post '/result' do
+  cents = params[:cents].to_i
+  result = min_coins(cents)
+  if result.has_key?(:quarter)
+    quarter = result.fetch_values(:quarter)
+  end
+  if result.has_key?(:dime)
+    dime = result.fetch_values(:dime)
+  end
+  if result.has_key?(:nickel)
+    nickel = result.fetch_values(:nickel)
+  end
+  if result.has_key?(:penny)
+    penny = result.fetch_values(:penny)
+  end
+  redirect '/coins?cents='+ cents.to_s + '&result='+ result.to_s + '&quarter='+ quarter.to_s + '&dime='+ dime.to_s + '&nickel='+ nickel.to_s + '&penny='+ penny.to_s
+end
+
+get '/coins' do
+  cents = params[:cents]
+  result = params[:result]
+  quarter = params[:quarter]
+  dime = params[:dime]
+  nickel = params[:nickel]
+  penny = params[:penny]
+  erb :coins, :layout => :layout, :locals => {result: result, cents: cents, quarter: quarter, dime: dime, nickel: nickel, penny: penny}
 end
--- coin.rb
+++ coin.rb
@@ -6,20 +6,20 @@ def min_coins(cents)
         if cents >= coins[:quarter]
             cents -= coins[:quarter]
             q_count += 1
-            used.store(:quarter, q_count)
         elsif cents >= coins[:dime]
             cents -= coins[:dime]
             d_count += 1
-            used.store(:dime, d_count)
         elsif cents >= coins[:nickel]
             cents -= coins[:nickel]
             n_count += 1
-            used.store(:nickel, n_count)
         elsif cents >= coins[:penny]
             cents -= coins[:penny]
             p_count += 1
-            used.store(:penny, p_count)
         end
+        used.store(:quarter, q_count)
+        used.store(:dime, d_count)
+        used.store(:nickel, n_count)
+        used.store(:penny, p_count)
     end
     used
 end
